fa552fa4d0b456e3a16219bef3d8dc75b5768cba
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-09-24 10:28:26 +0200

    account for refactoring of CounterReaderWriter

--- write_ahead_ticketer.go
+++ write_ahead_ticketer.go
@@ -1,63 +1,8 @@
 package surl
 
 import (
-	"encoding/binary"
 	"fmt"
-	"io/ioutil"
-	"os"
 )
-
-// CounterReaderWriter abstracts reading and writing of a single
-// uint64 counter value.
-type CounterReaderWriter interface {
-	// Read tries to read the counter value from the underlying storage solution.
-	// If there exists to previous value, defaultValue is returned.
-	Read(defaultValue uint64) (uint64, error)
-	// Write stores the value of counter to the underlying storage solution.
-	Write(counter uint64) error
-}
-
-// FileCounterReaderWriter implements CounterReaderWriter, relying on ordinary
-// files as the persistence backend.
-type FileCounterReaderWriter struct {
-	fn string
-}
-
-func (self *FileCounterReaderWriter) Read(defaultValue uint64) (uint64, error) {
-	value := defaultValue
-	if f, err := os.Open(self.fn); err == nil {
-		if err = binary.Read(f, binary.LittleEndian, &value); err != nil {
-			// There is a previous value that we have issues reading.
-			// With that, we would risk consistency and instead bail out
-			// here.
-			return value, err
-		}
-	}
-
-	return value, nil
-}
-
-func (self *FileCounterReaderWriter) Write(counter uint64) error {
-	f, err := ioutil.TempFile("", "SurlTemporaryState")
-
-	if err != nil {
-		return err
-	}
-
-	tempFileName := f.Name()
-	err = binary.Write(f, binary.LittleEndian, counter)
-	f.Close()
-
-	if err != nil {
-		return err
-	}
-
-	if err = os.Rename(tempFileName, self.fn); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 // WriteAheadTicketer implements Ticketer, maintaining a counter value
 // in a file, atomically updating it on calls to Next() prior to handing
